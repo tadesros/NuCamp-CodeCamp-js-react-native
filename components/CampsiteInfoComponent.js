c1b28f242ba9af88f0472e2b3df5b1a8556ddca4
Tom DesRosiers <tomadesrosiers@gmail.com>
2021-07-22 21:57:48 -0400

    added side nav

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
 function RenderCampsite({ campsite }) {
-    //Check if empty truthy 
+	//Check if empty truthy
 	if (campsite) {
 		return (
 			<Card
@@ -13,13 +14,33 @@ function RenderCampsite({ campsite }) {
 				<Text style={{ margin: 10 }}>{campsite.description}</Text>
 			</Card>
 		);
-    }
-    //Falsy not a valid campsite
+	}
+	//Falsy not a valid campsite
 	return <View />;
 }
 
-function CampsiteInfo(props) {
-	return <RenderCampsite campsite={props.campsite} />;
+class CampsiteInfo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			campsites: CAMPSITES,
+		};
+	}
+	//Set title
+	static navigationOptions = {
+		title: "Campsite Information",
+	};
+
+	render() {
+		//Receive the parameter id
+		const campsiteId = this.props.navigation.getParam("campsiteId");
+		//Pull out campsite object from array with filter
+		//Array index of zero
+		const campsite = this.state.campsites.filter(
+			(campsite) => campsite.id === campsiteId
+		)[0];
+		return <RenderCampsite campsite={campsite} />;
+	}
 }
 
 export default CampsiteInfo;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,36 +1,77 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
-import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { View } from "react-native";
+import Constants from "expo-constants";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 
+const DirectoryNavigator = createStackNavigator(
+	{
+		Directory: { screen: Directory },
+		CampsiteInfo: { screen: CampsiteInfo },
+	},
+	{
+		initialRouteName: "Directory",
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+		},
+	}
+);
+
+const HomeNavigator = createStackNavigator(
+	{
+		Home: { screen: Home },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+		},
+	}
+);
+
+//First parameter screen
+//second drawer background color
+const MainNavigator = createDrawerNavigator(
+	{
+		Home: { screen: HomeNavigator },
+		Directory: { screen: DirectoryNavigator },
+	},
+	{
+		drawerBackgroundColor: "#CEC8FF",
+	}
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null,
-        };
-    }
-
-    onCampsiteSelect(campsiteId) {
-        this.setState({ selectedCampsite: campsiteId });
-    }
-
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <Directory
-                    campsites={this.state.campsites}
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id === this.state.selectedCampsite)[0]}
-                />
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View
+				style={{
+					flex: 1,
+					paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+				}}
+			>
+				<AppNavigator />
+			</View>
+		);
+	}
 }
+
 export default Main;
